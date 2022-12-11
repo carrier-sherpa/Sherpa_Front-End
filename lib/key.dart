64221384704cd536c839be51fa35672c3bbeb812
hongjk7777 <51441261+hongjk7777@users.noreply.github.com>
2022-12-11 21:24:01 +0900

    Create key.dart

---
+++ lib/key.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Api {
+  static String KEY = 'YOUR_GOOGLE_MAP_API_KEY';
+  static String JSESSIONID = '';
+  static String ROOTURL = 'http://gamsagwa-env.eba-m4nqve9t.ap-northeast-2.elasticbeanstalk.com';
+}
